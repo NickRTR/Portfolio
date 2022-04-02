dd9d6bd9a66c12f5b49b10b5c5db8f492358468e
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-04-03 01:06:45 +0200

    Speeded up API call

--- src/routes/api/weather.json.js
+++ src/routes/api/weather.json.js
@@ -1,7 +1,7 @@
 import { API_KEY } from "$lib/api";
 
 export async function get() {
-    const result = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=Stuttgart`);
+    const result = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Stuttgart`);
     let data = await result.json();
 
     if (result.ok) {
