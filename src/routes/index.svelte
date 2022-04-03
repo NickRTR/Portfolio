f594ec62297abb78eedb5763d86fc4836fe35575
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-04-03 22:22:15 +0200

    Fixed typo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,7 +10,7 @@
             <img id="me" src="/me.jpg" alt="A picture of myself" title="A picture of myself">
             <section>
                 <p>
-                    I'm a self-thaught software engineer at the age of 16, located in Germany.<br>For the most part, I'm developing Web Apps with Sveltekit and Javascript. Additionally, I'm interested in anything that has to do with tech.
+                    I'm a self-taught software engineer at the age of 16, located in Germany.<br>For the most part, I'm developing Web Apps with Sveltekit and Javascript. Additionally, I'm interested in anything that has to do with tech.
                 </p>
                 <div class="links">
                     <a href="https://twitter.com/nickrtrrtr" target="_blank"><img class="social" src="/twitter.svg" alt="twitter" title="twitter"></a>
