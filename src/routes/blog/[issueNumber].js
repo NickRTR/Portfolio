69d31d07e3ef6f17986f9112c9f7d2eeb44c5594
NickRTR <nickrtrrtr@gmail.com>
2022-04-13 22:47:01 +0200

    Added Github API Authentication to get a higher api call limit

--- src/routes/blog/[issueNumber].js
+++ src/routes/blog/[issueNumber].js
@@ -1,5 +1,13 @@
-export async function get({ params }) {
-    const res = await fetch(`https://api.github.com/repos/nickrtr/portfolio/issues/${params.issueNumber}`);
+import { GH_HEADER } from "$lib/api";
+
+export async function get({ params }) {   
+    const res = await fetch(
+        `https://api.github.com/repos/nickrtr/portfolio/issues/${params.issueNumber}`,
+        {
+            headers: GH_HEADER
+        }
+    );
+
     const post = await res.json();
 
     return {
--- src/routes/blog/index.js
+++ src/routes/blog/index.js
@@ -1,5 +1,13 @@
-export async function get() {
-    const res = await fetch("https://api.github.com/repos/nickrtr/portfolio/issues?labels=post");
+import { GH_HEADER } from "$lib/api";
+
+export async function get() {   
+    const res = await fetch(
+        "https://api.github.com/repos/nickrtr/portfolio/issues?labels=post", 
+        {
+            headers: GH_HEADER
+        }
+    );
+
     const posts = await res.json();
 
     return {
