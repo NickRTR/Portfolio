b574b5256e6c2ebe5743ae2cc743e03abff5b211
NickRTR <nickrtrrtr@gmail.com>
2022-04-21 21:03:20 +0200

    style improvements

--- src/routes/skills.svelte
+++ src/routes/skills.svelte
@@ -1,7 +1,34 @@
 <script>
-
+    const skills = [
+        { title: "Javascript", src: "https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E" },
+        { title: "Svelte", src: "https://img.shields.io/badge/Svelte-4A4A55?style=for-the-badge&logo=svelte&logoColor=FF3E00" },
+        { title: "HTML", src: "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" },
+        { title: "CSS", src: "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" },
+    ]
 </script>
 
 <main>
     <h1>Skills</h1>
+
+    <div class="banners">
+        {#each skills as skill}
+            <img src={skill.src} alt={skill.title} title={skill.title}>
+        {/each}
+    </div>
 </main>
+
+<style>
+    .banners {
+        /* animation: slidingBanners 4s linear infinite; */
+    }
+
+    @keyframes slidingBanners {
+        0% {
+            transform: translateX(-100%);
+        }
+
+        100% {
+            transform: translateX(100%);
+        }
+    }
+</style>
--- src/routes/work/index.svelte
+++ src/routes/work/index.svelte
@@ -10,13 +10,13 @@
     <Heading text={["Work"]} />
 
     <ul>
-        <li><a class="linkUnderline" href="/work/Mikki" sveltekit:prefetch title="Mikki">Mikki</a></li>
+        <li><a class="linkUnderline" href="/work/Mikki" sveltekit:prefetch title="-> Mikki">Mikki</a></li>
             <ul><li>An assembler wiki created in SvelteKit</li></ul>
-        <li><a class="linkUnderline" href="/work/WeatherWatch" sveltekit:prefetch title="WeatherWatch">WeatherWatch</a></li>
+        <li><a class="linkUnderline" href="/work/WeatherWatch" sveltekit:prefetch title="-> WeatherWatch">WeatherWatch</a></li>
             <ul><li>Weather forecast and daily advice</li></ul>
-        <li><a class="linkUnderline" href="/work/Schoppy" sveltekit:prefetch title="Schoppy">Schoppy</a></li>
+        <li><a class="linkUnderline" href="/work/Schoppy" sveltekit:prefetch title="-> Schoppy">Schoppy</a></li>
             <ul><li>A simple yet powerful shopping list with themes, quantity, category icons, multiply languages and much more.</li></ul>
-        <li><a class="linkUnderline" href="/work/Carify" sveltekit:prefetch title="Carify">Carify</a></li>
+        <li><a class="linkUnderline" href="/work/Carify" sveltekit:prefetch title="-> Carify">Carify</a></li>
             <ul><li>Identify the origin of any car owner, in Germany and Austria, by the car's number plate.</li></ul>
     </ul>
 </main> 
@@ -24,14 +24,9 @@
 <style>
     main {
         font-size: 1.2rem;
-        text-align: left;
     }
 
     ul {
         margin: .25rem 0;
-    }
-
-    ul *::selection {
-        background-color: var(--yellow);
     }
 </style>
