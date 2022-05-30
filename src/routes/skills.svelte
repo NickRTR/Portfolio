55de9f22a8c95a10d39ae281bb27d5d05d30e8bb
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-05-30 21:42:23 +0200

    Added more skill banners

--- src/routes/skills.svelte
+++ src/routes/skills.svelte
@@ -1,4 +1,6 @@
 <script>
+    import Heading from "$lib/components/Heading.svelte"
+
     const skills = [
         // languages
         [
@@ -7,10 +9,19 @@
             { title: "Svelte", src: "https://img.shields.io/badge/Svelte-4A4A55?style=for-the-badge&logo=svelte&logoColor=FF3E00" },
             { title: "HTML ;)", src: "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" },
             { title: "CSS", src: "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" },
+            { title: "Markdown", src: "https://img.shields.io/badge/Markdown-000000?style=for-the-badge&logo=markdown&logoColor=white" },
             { title: "Java", src: "https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=java&logoColor=white" },
             { title: "Python", src: "https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue" },
             { title: "C++", src: "https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white" },
             { title: "Godot (GDScript)", src: "https://img.shields.io/badge/Godot-478CBF?style=for-the-badge&logo=GodotEngine&logoColor=white" },
+        ],
+        // framworks
+        [
+            { sectionName: "Frameworks" },
+            { title: "SvelteKit", src: "https://img.shields.io/badge/SvelteKit-FF3E00?style=for-the-badge&amp;logo=Svelte&amp;logoColor=white" },
+            { title: "Tauri", src: "https://img.shields.io/badge/Tauri-FFC131?style=for-the-badge&logo=Tauri&logoColor=white" },
+            { title: "Node.js", src: "https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white" },
+            { title: "Tailwind CSS", src: "https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white" },
         ],
         // databases
         [
@@ -25,18 +36,46 @@
             { title: "Figma", src: "https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white" },
             { title: "Gimp", src: "https://img.shields.io/badge/gimp-5C5543?style=for-the-badge&logo=gimp&logoColor=white" },
         ],
-        // framworks
+        // tooling
         [
-            { sectionName: "Frameworks" },
-            { title: "SvelteKit", src: "https://img.shields.io/badge/Supabase-181818?style=for-the-badge&logo=supabase&logoColor=white" },
-            { title: "Tauri", src: "https://img.shields.io/badge/Tauri-FFC131?style=for-the-badge&logo=Tauri&logoColor=white" },
-            { title: "Tailwind CSS", src: "https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white" },
+            { sectionName: "Tooling" },
+            { title: "Vite", src: "https://img.shields.io/badge/Vite-B73BFE?style=for-the-badge&logo=vite&logoColor=FFD62E" },
+            { title: "Vercel", src: "https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white" },
+            { title: "Netlify", src: "https://img.shields.io/badge/Netlify-00C7B7?style=for-the-badge&logo=netlify&logoColor=white" },
+            { title: "npm", src: "https://img.shields.io/badge/npm-CB3837?style=for-the-badge&logo=npm&logoColor=white" },
+        ],
+        // OS
+        [
+            { sectionName: "Operating system" },
+            { title: "Windows", src: "https://img.shields.io/badge/Windows-0078D6?style=for-the-badge&logo=windows&logoColor=white" },
+            { title: "Linux", src: "https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black" },
+            { title: "Pop!_OS", src: "https://img.shields.io/badge/Pop!_OS-48B9C7?style=for-the-badge&logo=Pop!_OS&logoColor=white" },
+            { title: "Ubuntu", src: "https://img.shields.io/badge/Ubuntu-E95420?style=for-the-badge&logo=ubuntu&logoColor=white" },
+            { title: "Manjaro", src: "https://img.shields.io/badge/manjaro-35BF5C?style=for-the-badge&logo=manjaro&logoColor=white" },
+            { title: "Fedora", src: "https://img.shields.io/badge/Fedora-294172?style=for-the-badge&logo=fedora&logoColor=white" },
+        ],
+        // office
+        [
+            { sectionName: "Office" },
+            { title: "Microsoft Office", src: "https://img.shields.io/badge/Microsoft_Office-D83B01?style=for-the-badge&logo=microsoft-office&logoColor=white" },
+            { title: "Libre Office", src: "https://img.shields.io/badge/LibreOffice-18A303?style=for-the-badge&logo=LibreOffice&logoColor=white" },
+            { title: "Notion", src: "https://img.shields.io/badge/Notion-000000?style=for-the-badge&logo=notion&logoColor=white" },
+            { title: "Obsidian", src: "https://img.shields.io/badge/Obsidian-483699?style=for-the-badge&amp;logo=Obsidian&amp;logoColor=white" },
+        ],
+        // further / other
+        [
+            { sectionName: "Further" },
+            { title: "Git", src: "https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white" },
+            { title: "GitHub", src: "https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" },
+            { title: "Visual Studio Code", src: "https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white" },
+            { title: "Arduino", src: "https://img.shields.io/badge/Arduino-00979D?style=for-the-badge&logo=Arduino&logoColor=white" },
+            { title: "WakaTime", src: "https://img.shields.io/badge/WakaTime-000000?style=for-the-badge&logo=WakaTime&logoColor=white" },
         ],
     ]
 </script>
 
 <main>
-    <h1>Skills</h1>
+    <Heading text={["Skills"]} />
 
     {#each skills as section}
         <section>
@@ -54,17 +93,19 @@
 </main>
 
 <style>
+    h2 {
+        margin-bottom: .4rem;
+    }
+
     .banner {
         border-radius: .5rem;
         border: 4px solid var(--yellow);
         padding: .5rem;
-        margin: 0;
         line-height: 0; /* remove padding at bottom */
     }
 
     img {
-        margin: 0;
-        padding: 0;
+        margin: .25rem;
     }
 
     @keyframes slidingBanners {
