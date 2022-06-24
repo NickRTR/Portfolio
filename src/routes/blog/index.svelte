246899365dfdf249c9837db1fcfc1d3592185ea0
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-06-24 23:05:26 +0200

    blog overview filter

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -2,6 +2,29 @@
     import Heading from "$lib/components/Heading.svelte";
 
     export let posts;
+
+    let filter = "";
+    let filteredPosts = posts;
+
+    function filterPosts() {
+        filteredPosts = [];
+        for (let i in posts) {
+            let post = posts[i];
+            if (post.title.toLowerCase().startsWith(filter.toLowerCase())) {
+                filteredPosts = [...filteredPosts, post];
+            }
+        }
+
+        // more wider search if no fitting post was found
+        if (filteredPosts.length === 0) {
+            for (let i in posts) {
+                let post = posts[i];
+                if (post.title.toLowerCase().includes(filter.toLocaleLowerCase())) {
+                    filteredPosts = [...filteredPosts, post];
+                }
+            }
+        }
+    }
 </script>
 
 <svelte:head>
@@ -11,20 +34,46 @@
 <!-- TODO: Revise Blog Post overview -->
 
 <main>
-    <Heading text={["Blog"]} />
+    <header>
+        <Heading text={["Blog"]} />
+        <input type="text" placeholder="filter" bind:value={filter} on:input={filterPosts}>
+    </header>
 
     {#if posts instanceof Array}
-        {#each posts as post}
-            <h1><a href={"/blog/" + post.number} rel="canonical" sveltekit:prefetch>{post.title}</a></h1>
-        {/each}
+        <div class="cardGrid">
+            {#each filteredPosts as post}
+                <div class="card">
+                    <h2><a href={"/blog/" + post.number} rel="canonical" sveltekit:prefetch>{post.title}</a></h2>
+                </div>
+            {/each}
+        </div>
     {:else}
         {posts.message}
     {/if}
 </main>
 
 <style>
-    h1 {
+    header {
+        text-align: center;
+    }
+
+    input {
+        margin-top: 1rem;
+    }
+    
+    h2 {
         margin-block: 1rem;
         line-height: 2.5rem;
+    }
+
+    .cardGrid {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+
+    .card {
+        margin: 1rem;
+        height: 20%;
+        background-color: red;
     }
 </style>
