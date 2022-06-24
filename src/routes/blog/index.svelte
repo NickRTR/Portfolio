e16745c111f86d15af711d9c0af02417a62bc0f2
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-06-24 23:23:08 +0200

    revised block post overview

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -31,18 +31,19 @@
     <title>Nick Reutlinger - Blog</title>
 </svelte:head>
 
-<!-- TODO: Revise Blog Post overview -->
-
 <main>
     <header>
         <Heading text={["Blog"]} />
         <input type="text" placeholder="filter" bind:value={filter} on:input={filterPosts}>
     </header>
 
+    <div class="divider"></div>
+
     {#if posts instanceof Array}
         <div class="cardGrid">
             {#each filteredPosts as post}
                 <div class="card">
+                    <p>{`${new Date(post.created_at).getDate()}.${new Date(post.created_at).getMonth()}.${new Date(post.created_at).getFullYear()}`}</p>
                     <h2><a href={"/blog/" + post.number} rel="canonical" sveltekit:prefetch>{post.title}</a></h2>
                 </div>
             {/each}
@@ -57,23 +58,50 @@
         text-align: center;
     }
 
-    input {
-        margin-top: 1rem;
+    .divider {
+        height: 10px;
+        border-radius: 2rem;
+        width: 100%;
+        background: linear-gradient(90deg, hsla(339, 100%, 55%, 1) 0%, hsla(197, 100%, 64%, 1) 100%);
     }
-    
-    h2 {
-        margin-block: 1rem;
-        line-height: 2.5rem;
+
+    input {
+        scroll-margin-block: 1rem;
+        border-radius: 5px;
+        outline: none;
+        border: 2px solid var(--textDark);
+        padding: .25rem .5rem;
+        font-size: 1rem;
+        transition: .1s ease-in-out;
+    }
+
+    input:focus, input:hover {
+        border: 2px solid var(--yellow);
     }
 
     .cardGrid {
+        margin-top: 1rem;
         display: grid;
         grid-template-columns: 1fr 1fr;
+        gap: 1rem;
     }
 
     .card {
-        margin: 1rem;
-        height: 20%;
-        background-color: red;
+        border: 5px solid var(--yellow);
+        border-radius: 1rem;
+        text-align: center;
+    }
+    
+    .card p {
+        padding-inline: .5rem;
+        background-color: var(--yellow);
+        margin: 0;
+    }
+
+    .card h2 {
+        word-break: keep-all;
+        border-bottom: none;
+        font-size: 1.25rem;
+        margin-inline: .5rem;
     }
 </style>
