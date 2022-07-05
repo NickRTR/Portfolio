4562172921f40d96d5ea561770accd7f0086b39e
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-05 17:38:29 +0200

    removed api.js from lib

--- src/lib/api.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const API_KEY = process.env.API_KEY;
-
-export const GH_HEADER = {
-    Authorization: `Token ${process.env.GH_TOKEN}`        
-}
