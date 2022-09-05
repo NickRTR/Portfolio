90c5c44002586398adab843b28a9c0db7ee2e6cb
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-09-05 18:23:01 +0200

    Fix About component typo

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,6 +1,7 @@
 <article>
 	<p>
-		I'm a self-taught software engineer at the age of 16, located in <mark>Germany</mark>.<br />For the most part, I'm developing Web Apps with <mark>Sveltekit</mark> and
+		I'm a self-taught software engineer at the age of 16, located in <mark>Germany</mark>.<br />
+		For the most part, I'm developing Web Apps with <mark>SvelteKit</mark> and
 		<mark>Javascript</mark>. Additionally, I'm interested in anything that has to do with
 		<mark>tech</mark>.
 	</p>
