0516f8bf08a9462d07f526be59fb14964fe91770
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-09-05 16:37:12 +0200

    style home page

---
+++ src/lib/components/About.svelte
@@ -0,0 +1,9 @@
+<article>
+	<p>I'm a self-taught software engineer at the age of 16, located in Germany. For the most part, I'm developing Web Apps with Sveltekit and Javascript. Additionally, I'm interested in anything that has to do with tech.</p>
+</article>
+
+<style>
+	article {
+		max-width: 350px;
+	}
+</style>
