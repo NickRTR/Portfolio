713a86fded97a9690224deab9ce411a754f159c5
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-11-09 16:04:23 +0100

    Change thanks screen link message

--- src/routes/thanks/+page.svelte
+++ src/routes/thanks/+page.svelte
@@ -1,4 +1,4 @@
 <main>
 	<h1>Thanks for submitting!</h1>
-	<a href="/" rel="canonical" title="-> Home">Go back to mommy...</a>
+	<a href="/" rel="canonical" title="-> Home">Go back home...</a>
 </main>
