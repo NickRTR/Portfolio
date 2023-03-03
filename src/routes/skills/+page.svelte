980ce47af442c44baee0beeeec9180f4b61393b9
NickRTR <95316598+NickRTR@users.noreply.github.com>
2023-03-03 22:08:48 +0100

    first Skills page design approach

---
+++ src/routes/skills/+page.svelte
@@ -0,0 +1,161 @@
+<script>
+</script>
+
+<main>
+	<h1>Skills</h1>
+
+	<article>
+		<aside>
+			<h3>Languages</h3>
+			<img
+				src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=000000"
+				alt="JavaScript"
+			/>
+			<img
+				src="https://img.shields.io/badge/go-00ADD8?style=for-the-badge&logo=go&logoColor=FFFFFF"
+				alt="Go"
+			/>
+			<img
+				src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"
+				alt="HTML"
+			/>
+			<img
+				src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"
+				alt="CSS"
+			/>
+			<img
+				src="https://img.shields.io/badge/Markdown-000000?style=for-the-badge&logo=markdown&logoColor=white"
+				alt="Markdown"
+			/>
+			<img
+				src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=java&logoColor=white"
+				alt="Java"
+			/>
+			<img
+				src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=Python&logoColor=FFFFFF"
+				alt="Python"
+			/>
+			<img
+				src="https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white"
+				alt="C++"
+			/>
+			<img
+				src="https://img.shields.io/badge/Godot-478CBF?style=for-the-badge&logo=GodotEngine&logoColor=white"
+				alt="Godot"
+			/>
+			<img
+				src="https://img.shields.io/badge/GraphQL-E10098?style=for-the-badge&logo=GraphQL&logoColor=FFFFFF"
+				alt="GraphQL"
+			/>
+		</aside>
+		<div />
+		<div />
+		<aside>
+			<h3>Languages</h3>
+			<img
+				src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=000000"
+				alt="JavaScript"
+			/>
+			<img
+				src="https://img.shields.io/badge/go-00ADD8?style=for-the-badge&logo=go&logoColor=FFFFFF"
+				alt="Go"
+			/>
+			<img
+				src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"
+				alt="HTML"
+			/>
+			<img
+				src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"
+				alt="CSS"
+			/>
+			<img
+				src="https://img.shields.io/badge/Markdown-000000?style=for-the-badge&logo=markdown&logoColor=white"
+				alt="Markdown"
+			/>
+			<img
+				src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=java&logoColor=white"
+				alt="Java"
+			/>
+			<img
+				src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=Python&logoColor=FFFFFF"
+				alt="Python"
+			/>
+			<img
+				src="https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white"
+				alt="C++"
+			/>
+			<img
+				src="https://img.shields.io/badge/Godot-478CBF?style=for-the-badge&logo=GodotEngine&logoColor=white"
+				alt="Godot"
+			/>
+			<img
+				src="https://img.shields.io/badge/GraphQL-E10098?style=for-the-badge&logo=GraphQL&logoColor=FFFFFF"
+				alt="GraphQL"
+			/>
+		</aside>
+		<aside>
+			<h3>Languages</h3>
+			<img
+				src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=000000"
+				alt="JavaScript"
+			/>
+			<img
+				src="https://img.shields.io/badge/go-00ADD8?style=for-the-badge&logo=go&logoColor=FFFFFF"
+				alt="Go"
+			/>
+			<img
+				src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"
+				alt="HTML"
+			/>
+			<img
+				src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"
+				alt="CSS"
+			/>
+			<img
+				src="https://img.shields.io/badge/Markdown-000000?style=for-the-badge&logo=markdown&logoColor=white"
+				alt="Markdown"
+			/>
+			<img
+				src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=java&logoColor=white"
+				alt="Java"
+			/>
+			<img
+				src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=Python&logoColor=FFFFFF"
+				alt="Python"
+			/>
+			<img
+				src="https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white"
+				alt="C++"
+			/>
+			<img
+				src="https://img.shields.io/badge/Godot-478CBF?style=for-the-badge&logo=GodotEngine&logoColor=white"
+				alt="Godot"
+			/>
+			<img
+				src="https://img.shields.io/badge/GraphQL-E10098?style=for-the-badge&logo=GraphQL&logoColor=FFFFFF"
+				alt="GraphQL"
+			/>
+		</aside>
+	</article>
+</main>
+
+<style>
+	article {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		width: 50%;
+		margin-inline: auto;
+		gap: 1rem;
+	}
+
+	aside {
+		border: 5px solid white;
+		border-radius: 1rem;
+		padding: 1rem;
+		padding-top: 0;
+	}
+
+	img {
+		border-radius: 0.5rem;
+	}
+</style>
