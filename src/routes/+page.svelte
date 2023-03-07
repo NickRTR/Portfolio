9ed0f2fd9c2607b80b3bcdc96807c93f4e8e380f
NickRTR <95316598+NickRTR@users.noreply.github.com>
2023-03-07 21:39:36 +0100

    First implementation of the about section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import ScrollIndicator from "$lib/components/ScrollIndicator.svelte";
+
+	export let data;
 </script>
 
 <main>
@@ -10,8 +12,10 @@
 		<ScrollIndicator />
 	</hero>
 
-	<article id="About">
-		<h1 id="about">About me</h1>
+	<article id="about">
+		<h1>About me</h1>
+		<img src="me.jpg" alt="myself" />
+		<section>{@html data.aboutPreview.about.text.html}</section>
 	</article>
 </main>
 
@@ -38,5 +42,23 @@
 		text-decoration-color: transparent;
 		color: var(--background);
 		background-color: var(--text);
+	}
+
+	#about {
+		min-height: 100vh;
+	}
+
+	#about h1 {
+		font-size: 2rem;
+	}
+
+	#about img {
+		width: 300px;
+		border-radius: 50%;
+	}
+
+	section {
+		max-width: 500px;
+		margin: auto;
 	}
 </style>
