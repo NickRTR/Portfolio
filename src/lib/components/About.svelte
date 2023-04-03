43e0efb6b7d15a8464aaf558d6d5cfc8c7789c79
NickRTR <95316598+NickRTR@users.noreply.github.com>
2023-04-03 22:48:32 +0200

    Update Project

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -2,8 +2,7 @@
 	<p>
 		I'm a self-taught software engineer at the age of 16, located in <mark>Germany</mark>.<br />
 		I develop Web Applications with <mark>SvelteKit</mark> and
-		<mark>Javascript</mark> as well as <mark>Go</mark>(lang) applications. Additionally, I'm
-		interested in anything that has to do with
+		<mark>Javascript</mark> as well as <mark>Go</mark>(lang) applications. Additionally, I'm interested in anything that has to do with
 		<mark>tech</mark>.
 	</p>
 </article>
