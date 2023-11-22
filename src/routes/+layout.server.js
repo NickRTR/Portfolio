97980d83903d81f5c43735d49dac275dfd56ff4a
Nick <95316598+NickRTR@users.noreply.github.com>
2023-11-22 19:09:43 +0100

    Try to fix analytics

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,5 +1,9 @@
 import { error } from "@sveltejs/kit";
 import dotenv from "dotenv";
+import { dev } from "$app/environment";
+import { inject } from "@vercel/analytics";
+
+inject({ mode: dev ? "development" : "production" });
 
 dotenv.config();
 
