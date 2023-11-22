8a2237b39eb3cf05271248a2f958245ca23b42fa
Nick <95316598+NickRTR@users.noreply.github.com>
2023-11-22 19:19:41 +0100

    Remove analytics

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 
 const config = {
-	plugins: [sveltekit()],
-	define: {
-		"import.meta.env.VERCEL_ANALYTICS_ID": JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
-	}
+	plugins: [sveltekit()]
 };
 
 export default config;
